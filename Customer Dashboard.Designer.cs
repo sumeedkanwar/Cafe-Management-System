913d339ac508f466204e824084157c7def1f5329
Sumeed Kanwar <sumeedkanwar@gmail.com>
2024-05-05 22:55:05 +0500

    done

--- Customer Dashboard.Designer.cs
+++ Customer Dashboard.Designer.cs
@@ -58,14 +58,13 @@
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(182, 90);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(136, 73);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 48;
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(363, 449);
+            this.dataGridView1.Size = new System.Drawing.Size(272, 365);
             this.dataGridView1.TabIndex = 13;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -73,9 +72,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 14F);
-            this.label1.Location = new System.Drawing.Point(177, 47);
+            this.label1.Location = new System.Drawing.Point(133, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 30);
+            this.label1.Size = new System.Drawing.Size(125, 26);
             this.label1.TabIndex = 16;
             this.label1.Text = "Browse Items";
             // 
@@ -86,10 +86,9 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI Variable Display", 10F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.SystemColors.Info;
-            this.button1.Location = new System.Drawing.Point(404, 559);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(303, 454);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 47);
+            this.button1.Size = new System.Drawing.Size(106, 38);
             this.button1.TabIndex = 17;
             this.button1.Text = "Add to Cart";
             this.button1.UseVisualStyleBackColor = false;
@@ -107,20 +106,18 @@
             this.panel3.Controls.Add(this.button2);
             this.panel3.Controls.Add(this.dataGridView2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(569, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(426, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(476, 690);
+            this.panel3.Size = new System.Drawing.Size(358, 561);
             this.panel3.TabIndex = 18;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI Variable Display", 14F);
-            this.label6.Location = new System.Drawing.Point(331, 47);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(248, 38);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(119, 30);
+            this.label6.Size = new System.Drawing.Size(102, 26);
             this.label6.TabIndex = 10;
             this.label6.Text = "12/34/1222";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -129,10 +126,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI Variable Text Semibold", 14.0177F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(269, 47);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(202, 38);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 31);
+            this.label5.Size = new System.Drawing.Size(58, 26);
             this.label5.TabIndex = 9;
             this.label5.Text = "Date:";
             // 
@@ -140,10 +136,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Variable Text Semibold", 14.0177F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(277, 556);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(208, 452);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 31);
+            this.label2.Size = new System.Drawing.Size(89, 26);
             this.label2.TabIndex = 6;
             this.label2.Text = "Total Bill:";
             // 
@@ -154,10 +149,9 @@
             this.Checkout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Checkout.Font = new System.Drawing.Font("Segoe UI Variable Display", 10F, System.Drawing.FontStyle.Bold);
             this.Checkout.ForeColor = System.Drawing.SystemColors.Info;
-            this.Checkout.Location = new System.Drawing.Point(308, 604);
-            this.Checkout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Checkout.Location = new System.Drawing.Point(231, 491);
             this.Checkout.Name = "Checkout";
-            this.Checkout.Size = new System.Drawing.Size(145, 46);
+            this.Checkout.Size = new System.Drawing.Size(109, 37);
             this.Checkout.TabIndex = 5;
             this.Checkout.Text = "Checkout";
             this.Checkout.UseVisualStyleBackColor = false;
@@ -167,10 +161,9 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI Variable Display", 14F);
-            this.label7.Location = new System.Drawing.Point(385, 556);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(289, 452);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(67, 30);
+            this.label7.Size = new System.Drawing.Size(57, 26);
             this.label7.TabIndex = 4;
             this.label7.Text = "$0.00";
             this.label7.Click += new System.EventHandler(this.label7_Click);
@@ -178,10 +171,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI Variable Display", 10F);
-            this.button2.Location = new System.Drawing.Point(25, 558);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(19, 453);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.Size = new System.Drawing.Size(75, 24);
             this.button2.TabIndex = 1;
             this.button2.Text = "Remove";
             this.button2.UseVisualStyleBackColor = true;
@@ -195,13 +187,12 @@
             this.dataGridView2.AllowUserToResizeRows = false;
             this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(25, 89);
-            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView2.Location = new System.Drawing.Point(19, 72);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowHeadersWidth = 48;
             this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView2.Size = new System.Drawing.Size(427, 449);
+            this.dataGridView2.Size = new System.Drawing.Size(320, 365);
             this.dataGridView2.TabIndex = 0;
             // 
             // panel2
@@ -213,9 +204,9 @@
             this.panel2.Controls.Add(this.label8);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(5);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(157, 690);
+            this.panel2.Size = new System.Drawing.Size(118, 561);
             this.panel2.TabIndex = 19;
             // 
             // button10
@@ -226,10 +217,9 @@
             this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button10.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 10.19469F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button10.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.button10.Location = new System.Drawing.Point(0, 603);
-            this.button10.Margin = new System.Windows.Forms.Padding(4);
+            this.button10.Location = new System.Drawing.Point(0, 490);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(157, 47);
+            this.button10.Size = new System.Drawing.Size(118, 38);
             this.button10.TabIndex = 28;
             this.button10.Text = "Profile";
             this.button10.UseVisualStyleBackColor = false;
@@ -243,10 +233,9 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 10.19469F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.button4.Location = new System.Drawing.Point(0, 644);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(0, 523);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(157, 47);
+            this.button4.Size = new System.Drawing.Size(118, 38);
             this.button4.TabIndex = 26;
             this.button4.Text = "Logout";
             this.button4.UseVisualStyleBackColor = false;
@@ -260,12 +249,11 @@
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 10.19469F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.button8.Location = new System.Drawing.Point(0, 59);
-            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Location = new System.Drawing.Point(0, 48);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(157, 47);
+            this.button8.Size = new System.Drawing.Size(118, 38);
             this.button8.TabIndex = 24;
-            this.button8.Text = "Give Feedbacks";
+            this.button8.Text = "Feedbacks";
             this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
@@ -273,23 +261,24 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI Variable Display", 14F);
-            this.label8.Location = new System.Drawing.Point(21, 10);
+            this.label8.Location = new System.Drawing.Point(16, 8);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(119, 30);
+            this.label8.Size = new System.Drawing.Size(103, 26);
             this.label8.TabIndex = 18;
             this.label8.Text = "Dashboard";
             // 
             // Customer_Dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1045, 690);
+            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Customer_Dashboard";
             this.Text = "Customer_Dashboard";
             this.Load += new System.EventHandler(this.Customer_Dashboard_Load);
